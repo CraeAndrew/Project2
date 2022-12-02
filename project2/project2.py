b6bb8e64c2f1a3c91588bf187d82c6cde174e20c
CraeAndrew <115107926+CraeAndrew@users.noreply.github.com>
2022-12-02 14:24:28 -0500

    Update project2.py

--- project2/project2.py
+++ project2/project2.py
@@ -6,7 +6,7 @@ from sensor_msgs.msg import Joy
 from std_msgs.msg import Int16
 from std_msgs.msg import Float32
 
-#from drive_interfaces.msg import VehCmd
+from drive_interfaces.msg import VehCmd
 
 from nav_msgs.msg import Odometry
 
@@ -15,62 +15,39 @@ from geometry_msgs.msg import PoseStamped
 
 import math
 
-class VehiclePose(Node):
+class AutoVeh(Node):
 
     def __init__(self):
         super().__init__('VehPose')
         
         self.subscription = self.create_subscription(
-            PoseStamped,'GPSData',self.listener_callback,10)
+            PoseStamped,'vehicle_pose',self.listener_callback,10)
         
         self.subscription2 = self.create_subscription(
-            Odometry,'odometry',self.listener_callback2,10)   
+            PoseStamped,'current_goal_pose',self.listener_callback2,10)   
 
         self.subscription  # prevent unused variable warning
         self.subscription2
         
-        self.x = 0.0
-        self.y = 0.0
-
-        self.theta = 0.0
-
-        self.publisher = self.create_publisher(PoseStamped, 'vehicle_pose', 10)
+        self.publisher = self.create_publisher(VehCmd, 'vehicle_command_angle', 10)
        
     def listener_callback(self, msg):
-        self.x = msg.pose.position.x
-        self.y = msg.pose.position.y
-
+        a=0
     def listener_callback2(self, msg):
-
-        v = msg.twist.twist.linear.x
-
-        z = msg.pose.pose.orientation.z
-        w = msg.pose.pose.orientation.w
-
-        self.theta = 2*math.atan2(z, w)
-
-        x_calc = self.x + v * math.cos(self.theta) * 0.15
-        y_calc = self.y + v * math.sin(self.theta) * 0.15
-        
-        msgEst = PoseStamped()
-        msgEst.pose.position.x = x_calc
-        msgEst.pose.position.y = y_calc
-        msgEst.pose.orientation.z = z
-        msgEst.pose.orientation.w = w
-
-        self.publisher.publish(msgEst)
+        b=0
+        #self.publisher.publish()
 
 def main(args=None):
     rclpy.init(args=args)
 
-    VehPose = VehiclePose()
+    avp = AutoVeh()
 
-    rclpy.spin(VehPose)
+    rclpy.spin(avp)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    VehPose.destroy_node()
+    avp.destroy_node()
     rclpy.shutdown()
 
 
